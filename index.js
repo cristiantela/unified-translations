d4b9c170dee2bf83ccbafb41dfa4db6e01980844
Matheus Cristian <mathues@email.com>
2022-10-13 12:02:35 -0300

    feat: added suggest feature

--- index.js
+++ index.js
@@ -3,6 +3,7 @@
 const fs = require("fs");
 const _ = require("lodash");
 const path = require("path");
+const translate = require('@vitalets/google-translate-api');
 
 function getArg(name) {
   if (!process.argv.includes(`--${name}`)) {
@@ -39,26 +40,46 @@ languages.forEach((language) => {
 });
 
 if (mode === "build") {
-  function readObject(object, path = []) {
-    Object.keys(object).forEach((key) => {
+  async function readObject(object, path = []) {
+    for (let key of Object.keys(object)) {
       if (object[key] instanceof Array || typeof object[key] === "string") {
         if (languages.includes(key)) {
           _.set(translations[key].data, path, object[key]);
         }
       } else if (object[key] instanceof Object) {
-        readObject(object[key], path.concat(key));
+        await readObject(object[key], path.concat(key));
+
+        if (getArg('suggest')) {
+          let [from, to] = getArg('suggest').split('>');
+
+          const googleFrom = from.split(':')[1] || from;
+          from = from.split(':')[0] || from;
+
+          const googleTo = to.split(':')[1] || to;
+          to = to.split(':')[0] || to;
+
+          if (object[key][from] && !object[key][to]) {
+            const response = await translate(object[key][from], { from: googleFrom, to: googleTo });
+
+            _.set(translations[to].data, path.concat(key), response.text.trim());
+
+            console.log(`${path.concat(key).join('.')} translated!`);
+          }
+        }
       }
-    });
+    }
   }
 
-  readObject(translationsFile);
+  (async () => {
+    await readObject(translationsFile);
 
-  languages.forEach((language) => {
-    fs.writeFileSync(
-      translations[language].output,
-      JSON.stringify(translations[language].data, null, "  ")
-    );
-  });
+    languages.forEach((language) => {
+      fs.writeFileSync(
+        translations[language].output,
+        JSON.stringify(translations[language].data, null, "  ")
+      );
+    });
+  })();
 } else if (mode === "reverse") {
   const translationsFile = {};
 
